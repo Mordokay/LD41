1c1d4aff07a3d0fc49e64da1e42978e5eafb1adf
Mordokay <papajorgioster@gmail.com>
2018-04-23 17:13:07 +0100

    More map corrections

---
+++ Assets/Scripts/RotateAround.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour {
+
+    public float rotationSpeed;
+
+	void Update () {
+        this.transform.Rotate(Vector3.up, Time.deltaTime * rotationSpeed);
+	}
+}
